c3e34b5ed1f0c9af2f716e480e3f9d9bf8ec032e
Anurag <adhangond@gmail.com>
2026-03-24 16:21:29 +0530

    ignore unwanted folders

--- src copy/app/members/member.model.ts
+++
@@ -1,8 +0,0 @@
-export class MemberData {
-  _id?: string;    // MongoDB ID
-  name: string = '';
-  email: string = '';
-  mobile: number = 0;
-  address: string = '';
-  services: string = '';
-}
--- src copy/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://society-backend-1pjv.onrender.com/api'
-};
